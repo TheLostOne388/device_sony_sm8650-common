6050e725bd16d9aa6f15280f4fa157fd968c578c
wxo <wxo@hotmail.com>
2025-03-11 21:54:35 +0700

    Daily update: 2025-03-11 21:54:35

--- common.mk
+++ common.mk
@@ -411,7 +411,6 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
-    android.hardware.sensors@2.1-service.multihal \
     libsensorndkbridge
 
 # Sensor configs
