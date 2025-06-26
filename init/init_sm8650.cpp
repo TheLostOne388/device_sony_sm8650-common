b487d3465366270a6eccc43db36dd3d4de2d6f84
wxo <wxo@hotmail.com>
2025-06-26 21:35:41 +0700

    avb issues
    Change-Id: Id32de13fd116c062bf9f4da7217d0e1664da0bf9

---
+++ init/init_sm8650.cpp
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2024 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <android-base/logging.h>
+
+void vendor_load_properties() {
+    // We have nothing to do here, but the init system expects this function to exist.
+} 
