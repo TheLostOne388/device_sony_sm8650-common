795abff1b293c0b70bcc5a82aba7df1a02757b67
wxo <wxo@hotmail.com>
2025-08-15 21:57:46 +0700

    Lots of sepolicy  work
    Change-Id: I11448b7acd82d19c21641d49527f5694a2e312e2

--- common.mk
+++ common.mk
@@ -45,7 +45,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl \
     android.hardware.soundtrigger@2.3-impl \
-    audio.bluetooth.default \
     audio.primary.pineapple \
     audio.r_submix.default \
     audio.usb.default \
